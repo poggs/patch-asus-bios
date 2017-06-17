96744f7277a9655f239adc0c99adf2870a8d8a8d
Peter Hicks <peter.hicks@poggs.co.uk>
2017-06-17 11:49:45 +0100

    Add additional SSDT tables

--- patch-bios.sh
+++ patch-bios.sh
@@ -9,10 +9,13 @@ sudo cat /sys/firmware/acpi/tables/DSDT  > dsdt.dat
 sudo cat /sys/firmware/acpi/tables/SSDT1 > ssdt1.dat
 sudo cat /sys/firmware/acpi/tables/SSDT2 > ssdt2.dat
 sudo cat /sys/firmware/acpi/tables/SSDT3 > ssdt3.dat
+sudo cat /sys/firmware/acpi/tables/dynamic/SSDT4 > ssdt4.dat
+sudo cat /sys/firmware/acpi/tables/dynamic/SSDT5 > ssdt5.dat
+sudo cat /sys/firmware/acpi/tables/dynamic/SSDT6 > ssdt6.dat
 
 echo "Decompiling"
 
-iasl -e ssdt1.dat -e ssdt2.dat -e ssdt3.dat -d dsdt.dat
+iasl -e ssdt1.dat -e ssdt2.dat -e ssdt3.dat -e ssdt4.dat -e ssdt5.dat -e ssdt6.dat -d dsdt.dat
 
 echo "Patching"
 
